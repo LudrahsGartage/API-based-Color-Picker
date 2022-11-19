b26e7a79a2b2dd3f40035592a49711349feba737
LudrahsGartage <101329074+LudrahsGartage@users.noreply.github.com>
2022-11-19 15:45:11 +0530

    finished

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const inputColor = document.getElementById("color")
 const colorScheme = document.getElementById("scheme")
 const fetchBtn = document.getElementById("fetch")
-const colorCollection = document.querySelector("main").children
+const colorContainer = document.querySelector("main")
 
-function fetchColors(colorInput,colorScheme) {
-    const url = `https://www.thecolorapi.com/scheme?hex=${colorInput}&mode=${colorScheme}`
-    console.log(colorInput)
-    console.log(colorScheme)
-    console.log(url)
+function setColors(colorInput,colorScheme) {
+    const url = `https://www.thecolorapi.com/scheme?hex=${colorInput.slice(1)}&mode=${colorScheme.toLowerCase()}`
     fetch(url)
         .then(res=>res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            render(data.colors)
+        })
 }
 
 
 fetchBtn.addEventListener("click", (e) => {
     e.preventDefault()
-    fetchColors(inputColor.value,colorScheme.value)})
+    setColors(inputColor.value, colorScheme.value)
+})
+
+function render(colors) {
+    const html = colors.map((color,i) => {
+        return (`
+        <div id="color-list-item-${i} data-color=${color.hex.value}">
+            <div class="color" data-color=${color.hex.value} style="background-color:${color.hex.value};"></div>
+            <p class="color-code" data-color=${color.hex.value}>${color.hex.value}</p>
+        </div>
+        `)
+    }).join("")
+    colorContainer.innerHTML = html
+}
+
+colorContainer.addEventListener("click", (e) => {
+    const colorClicked = e.target.dataset.color
+    if (colorClicked) {
+        navigator.clipboard.writeText(colorClicked)
+    }
+})
